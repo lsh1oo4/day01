ce720992cd43bc4731cca4748fdb5614055d6a76
lsh1oo4 <lsh1oo4@naver.com>
2019-05-21 11:06:37 +0900

    test2

--- test.js
+++ test.js
@@ -36,23 +36,38 @@
 // console.log("빼기 :"+calc(5,2)+"\n곱하기 :"+calc2(5,2));
 
 
-//4. 오브젝트
-var car = [
-    {
-        name: "first",
-        color: "yellow",
-        start:function(){
-            console.log("출발");
-        },
-        stop:function(){
-            console.log("멈춤");
-        }
-    },
-    {
-        name: "second",
-        color: "red"
-    }
-]
+// 4. 오브젝트 및 배열
+// var car = [];
+// var car1 = {
+//     name: "first",
+//     color: "yellow",
+//     start: function () {
+//         console.log("출발");
+//     },
+//     stop: function () {
+//         console.log("멈춤");
+//     }
+// };
+// var car2 = {
+//     name: "second",
+//     color: "red"
+// };
 
-console.log(car[0].name+" "+car[0].color);
-console.log(car[1].name+" "+car[1].color);
+// car[0] = car1;
+// car[1] = car2;
+
+// console.log(car[0].name + " " + car[0].color);
+// console.log(car[1].name + " " + car[1].color);
+
+//5. 반복문
+// var cars = ["BMW", "Volvo", "Saab", "Ford", "Fiat", "Audi"];
+// for (var i = 0; i < cars.length; i++) {
+//     console.log(cars[i]);
+// };
+
+// cars.forEach(function(str){     //foreach문
+//     console.log(str);
+// });
+
+
+//6. 조건문
